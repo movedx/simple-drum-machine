ecc8aba361abb5af98d10ab7e5369943b75d46a4
movedx <grytsenko.bogdan@gmail.com>
2021-03-28 21:23:11 +0200

    add freecodecamp tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return <div></div>;
 }
 
